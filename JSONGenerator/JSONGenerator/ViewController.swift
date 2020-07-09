ea7456c5ab4bc70ddb5364e56e68c22ece947704
Gustavo Vilas Boas <gustavovbs98@gmail.com>
2020-07-09 16:55:18 -0300

    Projeto criado

---
+++ JSONGenerator/JSONGenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JSONGenerator
+//
+//  Created by Gustavo Vilas Boas on 09/07/20.
+//  Copyright © 2020 Gustavo Vilas Boas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
